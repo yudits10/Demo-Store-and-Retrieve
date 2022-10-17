1b9c37a55a4cf17480103728d87bd57671e0820c
yudits10 <113966685+yudits10@users.noreply.github.com>
2022-10-18 04:04:22 +0700

    Add files via upload

---
+++ Demo Store and Retrieve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Store_and_Retrieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bob";
+            int messages = 3;
+            decimal temperature = 34.4m;
+
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.Write("! You have ");
+            Console.Write(messages);
+            Console.Write(" messages in your inbox. The temperature is ");
+            Console.Write(temperature);
+            Console.Write(" celsius.");
+        }
+    }
+}
